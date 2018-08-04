968e61a0e085537c0eb118c8e644f1fa15feb3ba
tnkTaka <taka.abc.hiko@gmail.com>
2018-08-05 00:24:26 +0900

    Initial commit

---
+++ app/src/main/java/local/hal/st32/android/mylibrary60213/LibraryListActivity.java
@@ -0,0 +1,13 @@
+package local.hal.st32.android.mylibrary60213;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LibraryListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_library_list);
+    }
+}
