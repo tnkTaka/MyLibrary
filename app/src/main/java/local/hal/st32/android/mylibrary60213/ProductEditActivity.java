25dede308c6d74690685252ad1095a5e979b909d
Unknown <taka.abc.hiko@gmail.com>
2018-08-08 06:48:40 +0900

    insertできた

--- app/src/main/java/local/hal/st32/android/mylibrary60213/ProductEditActivity.java
+++ app/src/main/java/local/hal/st32/android/mylibrary60213/ProductEditActivity.java
@@ -1,5 +1,7 @@
 package local.hal.st32.android.mylibrary60213;
 
+import android.app.DatePickerDialog;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,8 +11,16 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class ProductEditActivity extends AppCompatActivity {
 
@@ -19,9 +29,10 @@ public class ProductEditActivity extends AppCompatActivity {
     private ImageView IMAGE_VIEW;
     private Button CAMERA_BUTTON;
     private Spinner CATEGORY_SPINNER;
+    private EditText DEADLINE_EDIT_TEXT;
 
     private byte[] _byteImage;
-    private String _today;
+    private String _deadline;
     private String _category;
     private int _done;
 
@@ -30,8 +41,23 @@ public class ProductEditActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_edit);
 
+        // 今日日付の取得
+        Date now = new Date(System.currentTimeMillis());
+        DateFormat formatter = new SimpleDateFormat("yyyy年MM月dd");
+        _deadline = formatter.format(now);
+
         Intent intent = this.getIntent();
         Bitmap image = (Bitmap) intent.getParcelableExtra("Image");
+
+        // 画像の取り直しButton の紐付け
+        CAMERA_BUTTON = findViewById(R.id.camera_button);
+        CAMERA_BUTTON.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, RESULT_CAMERA);
+            }
+        });
 
         // 画像のBitMapをbyteに変換
         _byteImage = Tool.getToolBytes(image);
@@ -54,15 +80,10 @@ public class ProductEditActivity extends AppCompatActivity {
             }
         });
 
-        // 画像の取り直し　Button の紐付け
-        CAMERA_BUTTON = findViewById(R.id.camera_button);
-        CAMERA_BUTTON.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent, RESULT_CAMERA);
-            }
-        });
+        // 賞味期限EditText の紐付け
+        DEADLINE_EDIT_TEXT = findViewById(R.id.deadline_editText);
+        DEADLINE_EDIT_TEXT.setText(_deadline);
+
     }
 
     @Override
@@ -87,8 +108,54 @@ public class ProductEditActivity extends AppCompatActivity {
         }
     }
 
+    private class DatePickerDialogDateSetListener implements DatePickerDialog.OnDateSetListener{
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth){
+            String msg = + year + "年" + (monthOfYear + 1) + "月" + dayOfMonth + "日に変更しました";
+            Toast.makeText(ProductEditActivity.this, msg, Toast.LENGTH_SHORT).show();
+
+            DEADLINE_EDIT_TEXT.setText(String.format("%d年%02d月%02d日",year,monthOfYear + 1,dayOfMonth));
+        }
+    }
+
     public void onCreateButtonClick(View view) {
-        Log.d("result",""+_byteImage);
-        Log.d("result",""+_category);
+
+        _deadline = DEADLINE_EDIT_TEXT.getText().toString();
+
+        Log.d("Create",""+_byteImage);
+        Log.d("Create",_deadline);
+        Log.d("Create",_category);
+        Log.d("Create",""+_done);
+
+        DatabaseHelper helper = new DatabaseHelper(ProductEditActivity.this);
+        SQLiteDatabase db = helper.getWritableDatabase();
+        try {
+            DatabaseAccess.insert(db, _category, _deadline, _done, _byteImage);
+        } catch(Exception ex) {
+            Log.e("DB_ERROR", ex.toString());
+        } finally {
+            db.close();
+        }
+        finish();
+    }
+
+    public void onDateButtonClick(View view){
+        _deadline = DEADLINE_EDIT_TEXT.getText().toString();
+
+        if(_deadline.equals("")){
+            Calendar cal = Calendar.getInstance();
+            int nowYear = cal.get(Calendar.YEAR);
+            int nowMonth = cal.get(Calendar.MONTH);
+            int nowDayOfMonth = cal.get(Calendar.DAY_OF_MONTH);
+            DatePickerDialog dialog = new DatePickerDialog(ProductEditActivity.this, new DatePickerDialogDateSetListener(), nowYear,nowMonth,nowDayOfMonth);
+            dialog.show();
+        }else{
+            int dy = Integer.valueOf(_deadline.substring(0,4));
+            int dm = Integer.valueOf(_deadline.substring(5,7));
+            int dd = Integer.valueOf(_deadline.substring(8,10));
+            DatePickerDialog dialog = new DatePickerDialog(ProductEditActivity.this, new DatePickerDialogDateSetListener(),dy,dm -1 ,dd);
+            dialog.show();
+        }
+
     }
 }
--- app/src/main/java/local/hal/st32/android/mylibrary60213/ProductListActivity.java
+++ app/src/main/java/local/hal/st32/android/mylibrary60213/ProductListActivity.java
@@ -1,29 +1,54 @@
 package local.hal.st32.android.mylibrary60213;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.GridView;
+import android.widget.TextView;
 
 public class ProductListActivity extends AppCompatActivity {
 
     static final int MODE_INSERT = 1;
     static final int MODE_EDIT = 2;
+
     private final static int RESULT_CAMERA = 1001;
+
+    private GridView GRID_VIEW_IMAGE_TEXT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_list);
 
+        GRID_VIEW_IMAGE_TEXT = findViewById(R.id.grid_view_image_text);
+
     }
 
-    public void onNewButtonClick(View view) {
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(intent, RESULT_CAMERA);
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        DatabaseHelper helper = new DatabaseHelper(ProductListActivity.this);
+        SQLiteDatabase db = helper.getWritableDatabase();
+        Cursor cursor = DatabaseAccess.findAll(db);
+        Log.d("CursorMax",""+cursor.getCount());
+        if(cursor.moveToFirst()){
+            do {
+                long id = cursor.getLong(cursor.getColumnIndex("_id"));
+                String category = cursor.getString(cursor.getColumnIndex("category"));
+                String deadline = cursor.getString(cursor.getColumnIndex("deadline"));
+                Bitmap image = Tool.getToolImage(cursor.getBlob(cursor.getColumnIndex("image")));
+                Log.d("Cursor"+id,""+id+" : "+category+" : "+deadline+" : "+ image);
+            }while (cursor.moveToNext());
+        }
     }
 
     //これからImageViewにとった写真を張り付け。
@@ -51,5 +76,10 @@ public class ProductListActivity extends AppCompatActivity {
             intent.putExtra("Image", bitmap);
             startActivity(intent);
         }
+    }
+
+    public void onNewButtonClick(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, RESULT_CAMERA);
     }
 }
